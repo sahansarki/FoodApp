f518455bf6804727bdecd30e16cf9f5b105e57b7
sahansarki <sahancansarki@gmail.com>
2022-02-11 02:27:42 +0300

    Main Activity's location has been changed.

--- app/src/main/java/com/example/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.foodapp
+package com.example.foodapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.foodapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
