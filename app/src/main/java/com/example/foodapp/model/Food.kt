c7ca217b2fbc28a27074cef9ab2801c244748017
sahansarki <sahancansarki@gmail.com>
2022-02-11 02:31:01 +0300

    the Food data class was converted to parcelize to send data between fragments as a object parameter.

--- app/src/main/java/com/example/foodapp/model/Food.kt
+++ app/src/main/java/com/example/foodapp/model/Food.kt
@@ -1,8 +1,13 @@
 package com.example.foodapp.model
 
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Food(
     val yemek_id : String,
     val yemek_adi : String,
     val yemek_resim_adi : String,
     val yemek_fiyat : String
-)
+): Parcelable
