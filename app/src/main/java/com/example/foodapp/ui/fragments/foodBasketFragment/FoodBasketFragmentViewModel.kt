ffadc95a8df175bff1d45cba44dfe0b2c8059eba
sahansarki <sahancansarki@gmail.com>
2022-02-11 21:00:58 +0300

    HiltViewModel annotation was added and constructor parameters are being injected.

--- app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
@@ -1,8 +1,14 @@
 package com.example.foodapp.ui.fragments.foodBasketFragment
 
 import com.example.foodapp.base.BaseViewModel
+import com.example.foodapp.repository.FoodRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class FoodBasketFragmentViewModel(): BaseViewModel() {
+@HiltViewModel
+class FoodBasketFragmentViewModel @Inject constructor(
+    private val foodRepository: FoodRepository
+): BaseViewModel() {
 
 
 
