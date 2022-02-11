751a6b303650b21d1010df6f31c485e01e26af1f
sahansarki <sahancansarki@gmail.com>
2022-02-11 21:00:06 +0300

    Adapter has been binded to the fragment foodList state is being observed from fragment

--- app/src/main/java/com/example/foodapp/ui/fragments/foodListFragment/FoodListFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodListFragment/FoodListFragment.kt
@@ -2,11 +2,19 @@ package com.example.foodapp.ui.fragments.foodListFragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.foodapp.R
 import com.example.foodapp.base.BaseFragment
 import com.example.foodapp.databinding.FragmentFoodListBinding
+import com.example.foodapp.enums.RepositoryStatus
+import com.example.foodapp.ui.adapter.recyclerview.FoodListRecyclerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class FoodListFragment: BaseFragment<FragmentFoodListBinding>(FragmentFoodListBinding::inflate) {
 
     companion object {
@@ -14,13 +22,48 @@ class FoodListFragment: BaseFragment<FragmentFoodListBinding>(FragmentFoodListBi
         fun newInstance() = FoodListFragment()
     }
 
+    private val foodListFragmentViewModel: FoodListFragmentViewModel by viewModels()
+    private lateinit var foodListAdapter: FoodListRecyclerAdapter
+
     override fun getLayoutResId(): Int = R.layout.fragment_food_list
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        foodListAdapter = FoodListRecyclerAdapter(arrayListOf())
+
+        with(fragmentDataBinding.foodListRecyclerView) {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            setHasFixedSize(true)
+            adapter = foodListAdapter
+        }
+        foodListFragmentViewModel.getAllFoods()
+        observeFoodList()
     }
 
     override fun onResume() {
         super.onResume()
+    }
+
+    private fun observeFoodList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                foodListFragmentViewModel.foodList.collect {
+                    when(it.status){
+                        RepositoryStatus.LOADING -> {
+
+                        }
+
+                        RepositoryStatus.OK -> {
+                            foodListAdapter.setFoodList(it.data!!.yemekler)
+                        }
+
+                        RepositoryStatus.ERROR -> {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
