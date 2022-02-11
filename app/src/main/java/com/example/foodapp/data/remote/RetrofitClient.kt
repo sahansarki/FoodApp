f194111ae20af4227529bf276c8ee3a9485996f9
sahansarki <sahancansarki@gmail.com>
2022-02-11 21:05:35 +0300

    the BASE_URL's location was changed to Constans package

--- app/src/main/java/com/example/foodapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/foodapp/data/remote/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.example.foodapp.data.remote
 
+import com.example.foodapp.utils.Constants.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,7 +9,7 @@ abstract class RetrofitClient {
     companion object{
 
         private var INSTANCE: Retrofit? = null
-        private const val BASE_URL = "http://kasimadalan.pe.hu/yemekler/"
+
 
         fun getInstance(): Retrofit{
             return INSTANCE ?: synchronized(this){
