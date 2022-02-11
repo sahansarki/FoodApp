dfa7a55c186466f482cad5ccb3a291130450a72c
sahansarki <sahancansarki@gmail.com>
2022-02-11 21:00:46 +0300

    HiltViewModel annotation was added and constructor parameters are being injected.

--- app/src/main/java/com/example/foodapp/ui/fragments/foodListFragment/FoodListFragmentViewModel.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodListFragment/FoodListFragmentViewModel.kt
@@ -4,13 +4,17 @@ import androidx.lifecycle.viewModelScope
 import com.example.foodapp.base.BaseViewModel
 import com.example.foodapp.data.remote.reqres.FoodsResponse
 import com.example.foodapp.data.repository.FoodRepositoryImpl
+import com.example.foodapp.repository.FoodRepository
 import com.example.foodapp.utils.DataHolder
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FoodListFragmentViewModel(
-    private val foodRepositoryImpl: FoodRepositoryImpl
+@HiltViewModel
+class FoodListFragmentViewModel @Inject constructor(
+    private val foodRepositoryImpl: FoodRepository
 ): BaseViewModel() {
 
     private val mutableFoodList =  MutableStateFlow<DataHolder<FoodsResponse>>(DataHolder.loading())
@@ -19,6 +23,7 @@ class FoodListFragmentViewModel(
 
     fun getAllFoods(){
         viewModelScope.launch(Dispatchers.Main) {
+
             mutableFoodList.value = DataHolder.success(foodRepositoryImpl.getAllFoods())
         }
     }
