d6f55eeaa865b09ab2ada2f9fd14fbd641110c51
sahansarki <sahancansarki@gmail.com>
2022-02-11 21:04:40 +0300

    URL'S for internet requests were created/

---
+++ app/src/main/java/com/example/foodapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.foodapp.utils
+
+object Constants {
+    const val BASE_URL = "http://kasimadalan.pe.hu/yemekler/"
+    const val FOOD_PHOTOS_URL = "http://kasimadalan.pe.hu/yemekler/resimler/"
+}
