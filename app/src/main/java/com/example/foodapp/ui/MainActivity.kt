fac49e1b4065661e75b1fc8c31571de44a5876d2
sahansarki <sahancansarki@gmail.com>
2022-02-12 19:25:39 +0300

    bottom navigation was binded to navigation graph

--- app/src/main/java/com/example/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.foodapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.foodapp.R
+import com.example.foodapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = host.navController
+        setUpBottomNavMenu(navController)
+    }
+
+    private fun setUpBottomNavMenu(navController: NavController) {
+        val bottomNav = activityMainBinding.bottomNavigation
+        bottomNav.setupWithNavController(navController)
     }
 }
