acaabbf2caf3533603eda91de096fe918c43ad51
sahansarki <sahancansarki@gmail.com>
2022-02-13 21:12:17 +0300

    user_id was changed.

--- app/src/main/java/com/example/foodapp/utils/Constants.kt
+++ app/src/main/java/com/example/foodapp/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.example.foodapp.utils
 object Constants {
     const val BASE_URL = "http://kasimadalan.pe.hu/yemekler/"
     const val FOOD_PHOTOS_URL = "http://kasimadalan.pe.hu/yemekler/resimler/"
-    const val USER_ID = "tt"
+    const val USER_ID = "deneme"
 }
