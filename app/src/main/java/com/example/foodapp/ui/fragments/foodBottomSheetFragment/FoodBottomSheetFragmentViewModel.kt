cf70afa04c358f6668475cd41d4fa65ebcd026ab
sahansarki <sahancansarki@gmail.com>
2022-02-13 16:43:56 +0300

    viewmodel for bottom sheet fragment was created.

---
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodBottomSheetFragment/FoodBottomSheetFragmentViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.foodapp.ui.fragments.foodBottomSheetFragment
+
+import androidx.lifecycle.viewModelScope
+import com.example.foodapp.base.BaseViewModel
+import com.example.foodapp.data.remote.reqres.FoodsFromBasketResponse
+import com.example.foodapp.model.Food
+import com.example.foodapp.repository.FoodRepository
+import com.example.foodapp.utils.Constants
+import com.example.foodapp.utils.DataHolder
+import com.example.foodapp.utils.FoodError
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FoodBottomSheetFragmentViewModel @Inject constructor(
+    private val foodRepository: FoodRepository
+): BaseViewModel() {
+
+    private val mutableFoodList =  MutableStateFlow<DataHolder<FoodsFromBasketResponse>>(DataHolder.loading())
+    val foodList: MutableStateFlow<DataHolder<FoodsFromBasketResponse>>
+        get() = mutableFoodList
+
+    fun addFoodToBasket(food: Food, total: Int){
+        viewModelScope.launch {
+            foodRepository.addFoodToBasket(food.yemek_adi,food.yemek_resim_adi,food.yemek_fiyat.toInt(),total,Constants.USER_ID)
+        }
+    }
+
+
+    fun deleteFoodFromBasket(basket_food_id: Int, user_id: String){
+        viewModelScope.launch {
+            foodRepository.deleteFoodFromBasket(basket_food_id, user_id)
+        }
+    }
+    fun getFoodsFromBasket(user_id: String){
+        viewModelScope.launch(Dispatchers.Main) {
+            val foodsFromBasketResponse = foodRepository.getFoodsFromBasket(user_id)
+            if(foodsFromBasketResponse.success.toInt() != 0) mutableFoodList.value = DataHolder.success(foodRepository.getFoodsFromBasket(user_id))
+            else mutableFoodList.value = DataHolder.error(FoodError("sepet boş"), null)
+        }
+
+    }
+}
