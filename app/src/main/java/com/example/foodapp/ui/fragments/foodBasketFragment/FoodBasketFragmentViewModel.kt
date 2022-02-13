a2e04172550666fcef9734d2f21dca357d5a30b9
sahansarki <sahancansarki@gmail.com>
2022-02-13 21:14:53 +0300

    delete action has been added.

--- app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
@@ -2,12 +2,15 @@ package com.example.foodapp.ui.fragments.foodBasketFragment
 
 import androidx.lifecycle.viewModelScope
 import com.example.foodapp.base.BaseViewModel
+import com.example.foodapp.data.remote.reqres.DeleteFoodFromBasketResponse
 import com.example.foodapp.data.remote.reqres.FoodsFromBasketResponse
 import com.example.foodapp.data.remote.reqres.FoodsResponse
 import com.example.foodapp.repository.FoodRepository
 import com.example.foodapp.utils.DataHolder
+import com.example.foodapp.utils.FoodError
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,8 +25,18 @@ class FoodBasketFragmentViewModel @Inject constructor(
         get() = mutableFoodList
 
     fun getFoodsFromBasket(user_id: String){
-        viewModelScope.launch(Dispatchers.Main) {
-            mutableFoodList.value = DataHolder.success(foodRepository.getFoodsFromBasket(user_id))
+       viewModelScope.launch(Dispatchers.Main) {
+            val foodsFromBasketResponse = foodRepository.getFoodsFromBasket(user_id)
+            mutableFoodList.value = foodsFromBasketResponse
+        }
+    }
+
+    fun deleteFoodFromBasket(basket_food_id: Int, user_id: String){
+        viewModelScope.launch {
+            val deferred = async(Dispatchers.IO){
+                foodRepository.deleteFoodFromBasket(basket_food_id,user_id)
+            }
+            deferred.await()
         }
     }
 
