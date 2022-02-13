c946db96be8b3b3849b571752cba2c9172edd975
sahansarki <sahancansarki@gmail.com>
2022-02-13 16:41:13 +0300

    annotations for retrofit have been added.

--- app/src/main/java/com/example/foodapp/data/remote/reqres/FoodsFromBasketResponse.kt
+++ app/src/main/java/com/example/foodapp/data/remote/reqres/FoodsFromBasketResponse.kt
@@ -1,6 +1,14 @@
 package com.example.foodapp.data.remote.reqres
 
+import com.example.foodapp.model.FoodBasket
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 data class FoodsFromBasketResponse(
-    val success: Int,
-    val message: String
+    @SerializedName("sepet_yemekler")
+    @Expose
+    val sepet_yemekler: List<FoodBasket>,
+    @SerializedName("success")
+    @Expose
+    val success: String,
 )
