f935260f8a9ebfd7d1af0904399b3c435e74b14a
sahansarki <sahancansarki@gmail.com>
2022-02-13 16:47:25 +0300

    viewmodel for basket screen was created.

--- app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragmentViewModel.kt
@@ -1,14 +1,31 @@
 package com.example.foodapp.ui.fragments.foodBasketFragment
 
+import androidx.lifecycle.viewModelScope
 import com.example.foodapp.base.BaseViewModel
+import com.example.foodapp.data.remote.reqres.FoodsFromBasketResponse
+import com.example.foodapp.data.remote.reqres.FoodsResponse
 import com.example.foodapp.repository.FoodRepository
+import com.example.foodapp.utils.DataHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FoodBasketFragmentViewModel @Inject constructor(
     private val foodRepository: FoodRepository
 ): BaseViewModel() {
+
+    private val mutableFoodList =  MutableStateFlow<DataHolder<FoodsFromBasketResponse>>(DataHolder.loading())
+    val foodList: MutableStateFlow<DataHolder<FoodsFromBasketResponse>>
+        get() = mutableFoodList
+
+    fun getFoodsFromBasket(user_id: String){
+        viewModelScope.launch(Dispatchers.Main) {
+            mutableFoodList.value = DataHolder.success(foodRepository.getFoodsFromBasket(user_id))
+        }
+    }
 
 
 
