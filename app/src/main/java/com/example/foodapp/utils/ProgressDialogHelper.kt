afde452e2695c66b6122baf53638cad0630fc00f
sahansarki <sahancansarki@gmail.com>
2022-02-13 23:32:07 +0300

    to show progress bar xml and helper class were created.

---
+++ app/src/main/java/com/example/foodapp/utils/ProgressDialogHelper.kt
@@ -0,0 +1,34 @@
+package com.example.foodapp.utils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import com.example.foodapp.R
+
+object ProgressDialogHelper {
+
+    private var progressDialog: AlertDialog? = null
+
+    fun showProgress(context: Context) {
+
+        if (progressDialog != null) {
+            return
+        }
+
+        val builder = AlertDialog.Builder(context)
+        val newFeaturesView = LayoutInflater.from(context).inflate(R.layout.dialog_progress, null)
+
+        builder.setView(newFeaturesView)
+        progressDialog = builder.create()
+        progressDialog!!.show()
+        progressDialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    fun dismiss() {
+        progressDialog?.dismiss()
+        progressDialog = null
+    }
+
+}
