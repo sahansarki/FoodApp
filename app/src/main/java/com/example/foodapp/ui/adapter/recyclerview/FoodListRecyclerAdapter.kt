07e2879d75ac9344f73bbaf1a22a94d736bd2e4c
sahansarki <sahancansarki@gmail.com>
2022-02-13 16:43:05 +0300

    the type of code has been changed. (Ctrl + Space + L)

--- app/src/main/java/com/example/foodapp/ui/adapter/recyclerview/FoodListRecyclerAdapter.kt
+++ app/src/main/java/com/example/foodapp/ui/adapter/recyclerview/FoodListRecyclerAdapter.kt
@@ -8,18 +8,18 @@ import com.example.foodapp.ui.adapter.viewholder.FoodListViewHolder
 class FoodListRecyclerAdapter(
     private val foodList: ArrayList<Food>,
     private val orderFood: (food: Food) -> Unit
-): RecyclerView.Adapter<FoodListViewHolder> (){
+) : RecyclerView.Adapter<FoodListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodListViewHolder {
         return FoodListViewHolder.create(parent)
     }
 
     override fun onBindViewHolder(holder: FoodListViewHolder, position: Int) {
-        holder.bind(foodList[position],orderFood)
+        holder.bind(foodList[position], orderFood)
     }
 
     override fun getItemCount(): Int = foodList.size
 
-    fun setFoodList(items: List<Food>){
+    fun setFoodList(items: List<Food>) {
         foodList.clear()
         foodList.addAll(items)
         notifyDataSetChanged()
