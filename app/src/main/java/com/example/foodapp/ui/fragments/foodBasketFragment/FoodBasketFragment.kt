2e4c133c48f775d6b23140b4afc73292dee5e81c
sahansarki <sahancansarki@gmail.com>
2022-02-13 16:47:05 +0300

    to List basket items , recyclerview was created and binded to fragment.

--- app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/foodBasketFragment/FoodBasketFragment.kt
@@ -2,10 +2,23 @@ package com.example.foodapp.ui.fragments.foodBasketFragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.foodapp.R
 import com.example.foodapp.base.BaseFragment
 import com.example.foodapp.databinding.FragmentFoodBasketBinding
+import com.example.foodapp.enums.RepositoryStatus
+import com.example.foodapp.ui.adapter.recyclerview.FoodBasketListRecyclerAdapter
+import com.example.foodapp.ui.adapter.recyclerview.FoodListRecyclerAdapter
+import com.example.foodapp.ui.fragments.foodListFragment.FoodListFragmentViewModel
+import com.example.foodapp.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FoodBasketFragment: BaseFragment<FragmentFoodBasketBinding>(FragmentFoodBasketBinding::inflate) {
@@ -16,10 +29,49 @@ class FoodBasketFragment: BaseFragment<FragmentFoodBasketBinding>(FragmentFoodBa
         fun newInstance() = FoodBasketFragment()
     }
 
+    private lateinit var foodListAdapter: FoodBasketListRecyclerAdapter
+    private val foodBasketFragmentViewModel: FoodBasketFragmentViewModel by viewModels()
+
     override fun getLayoutResId(): Int = R.layout.fragment_food_basket
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        foodListAdapter = FoodBasketListRecyclerAdapter(arrayListOf())
+
+        with(fragmentDataBinding.foodBasketListRecyclerView) {
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            setHasFixedSize(true)
+            adapter = foodListAdapter
+        }
+        foodBasketFragmentViewModel.getFoodsFromBasket(Constants.USER_ID)
+        observeBasketFoodList()
+    }
+
+    private fun observeBasketFoodList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                foodBasketFragmentViewModel.foodList.collect {
+                    when(it.status){
+                        RepositoryStatus.LOADING -> {
+
+                        }
+
+                        RepositoryStatus.OK -> {
+                            foodListAdapter.setFoodList(it.data!!.sepet_yemekler)
+                        }
+
+                        RepositoryStatus.ERROR -> {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onResume() {
