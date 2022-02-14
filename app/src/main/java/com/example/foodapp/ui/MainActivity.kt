5651dcf116ca9ac0d263c524113706f53befb861
sahansarki <sahancansarki@gmail.com>
2022-02-14 22:37:13 +0300

    splash screen starting code was added.

--- app/src/main/java/com/example/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.foodapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
